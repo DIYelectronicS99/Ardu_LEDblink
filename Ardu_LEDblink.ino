ffa1b197a42b22c22c8916bc553f16cb756c4f1a
DIYelectronicS <30397750+DIYelectronicS99@users.noreply.github.com>
2017-07-24 08:51:51 +0530

    Add files via upload

---
+++ Ardu_LEDblink.ino
@@ -0,0 +1,16 @@
+#define led 7
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+}
+
+
+void loop()
+{
+  digitalWrite(led,HIGH); // turns the led on
+  delay(1000); // wait for 1 second
+  digitalWrite(led, LOW); // turns the led off
+  delay(1000); //wait for 1 second
+}
+
